de6ee39deb8d00016ef05f91c556f69049af9565
MuratcanKaynak <muratcan.kaynak39@gmail.com>
2023-05-18 12:36:52 +0300

    oyun-5

---
+++ PlatformGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public UIManager uimanager;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        CoinCalculator(0);
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && gameObject.CompareTag("FinishLine"))
+        {
+            CoinCalculator(100);
+            uimanager.CoinTextUpdate();
+            uimanager.FinishScreen();
+        }
+    }
+
+    public void CoinCalculator(int money)
+    {
+        if (PlayerPrefs.HasKey("moneyy"))
+        {
+            int oldScore = PlayerPrefs.GetInt("moneyy");
+            PlayerPrefs.SetInt("moneyy", oldScore + money);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("moneyy", 0);
+        }
+    }
+}
